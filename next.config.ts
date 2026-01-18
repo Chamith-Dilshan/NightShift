a7122ac9be377925a00755ffc93f7aa4dc6033eb
Chamith Dilshan <chamithzdilshann@gmail.com>
2026-01-18 11:51:20 +0530

    Initial project structure

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // Ensure Next.js uses SSG instead of SSR
+  // https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
+  output: "export",
 };
 
 export default nextConfig;
